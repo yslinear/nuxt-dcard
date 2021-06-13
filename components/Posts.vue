6b4d7f649d24b33fec00edde684bb805bada6220
yslinear <yslinear@gmail.com>
2021-06-14 05:27:55 +0800

    perf: :lipstick: 修改版面

--- components/Posts.vue
+++ components/Posts.vue
@@ -1,25 +1,45 @@
 <template>
   <v-list three-line>
-    <v-list-item-group
-      v-model="selectedPost"
-      :color="this.$vuetify.theme.dark ? 'dark' : 'primary'"
-    >
-      <template v-for="(item, index) in posts">
-        <v-list-item :key="index">
-          <v-list-item-avatar>
-            <v-img :src="item.avatar"></v-img>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-subtitle
-              v-html="item.forumName + '・' + item.school"
-            ></v-list-item-subtitle>
-            <v-list-item-title v-html="item.title"></v-list-item-title>
-            <v-list-item-subtitle v-html="item.excerpt"></v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <v-divider v-if="index < posts.length - 1" :key="index"></v-divider>
-      </template>
-    </v-list-item-group>
+    <template v-for="(item, index) in posts">
+      <v-list-item :key="item.id">
+        <v-list-item-content>
+          <v-row class="mx-lg-16">
+            <v-col :cols="item.mediaMeta['0'] !== undefined ? '8' : '12'">
+              <v-list-item-subtitle
+                v-html="
+                  item.forumName +
+                  '・' +
+                  (item.school !== undefined ? item.school : '匿名')
+                "
+              ></v-list-item-subtitle>
+              <v-list-item-title
+                class="my-2 text-pre-wrap"
+                v-html="item.title"
+              ></v-list-item-title>
+              <v-list-item-subtitle
+              class="text-pre-wrap"
+                v-html="item.excerpt"
+              ></v-list-item-subtitle>
+            </v-col>
+            <v-col
+              class="tw-grid"
+              v-if="item.mediaMeta['0'] !== undefined"
+              cols="4"
+            >
+              <v-img
+                class="rounded"
+                position="center center"
+                :lazy-src="item.mediaMeta['0'].thumbnail"
+                :src="item.mediaMeta['0'].thumbnail"
+                aspect-ratio="1"
+                max-width="250"
+              ></v-img>
+            </v-col>
+          </v-row>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider v-if="index < posts.length - 1" :key="index"></v-divider>
+    </template>
   </v-list>
 </template>
 
@@ -29,7 +49,9 @@ export default {
     return { posts: [], selectedPost: -1 };
   },
   async fetch() {
-    this.posts = await this.$axios.$get("/service/api/v2/posts?popular=true&limit=100");
+    this.posts = await this.$axios.$get(
+      "/service/api/v2/posts?popular=true&limit=100"
+    );
   },
   // call fetch only on client-side
   fetchOnServer: true,
